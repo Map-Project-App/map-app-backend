b38dabfae5dbb6032c324e65a39a692b9b854b22
Alan Chen <alanchen1908@gmail.com>
2021-06-10 23:04:09 -0600

    Minor workflow patch

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -9,12 +9,6 @@ cleanup() {
 
 trap cleanup SIGINT EXIT
 
-# Make sure docker-compose is installed
-if ! hash docker-compose 2>/dev/null; then
-  echo -e '\033[0;31mPlease install docker-compose\033[0m'
-  exit 1
-fi
-
 if [ -z "$(docker network ls -qf name=^entropic$)" ]; then
   echo "Creating network"
   docker network create entropic >/dev/null
